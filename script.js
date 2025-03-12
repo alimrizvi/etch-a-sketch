89949c716a5e3508878e21825193026d55003ab2
alimrizvi <62912143+alimrizvi@users.noreply.github.com>
2025-03-12 17:22:32 +0500

    Fixed bug with slider not working post-reset

--- script.js
+++ script.js
@@ -7,24 +7,6 @@ const changeColor = function (e) {
     }
 }
 
-const drawBoard = function (boardLength) {
-    const board = document.createElement('div')
-    board.classList.add('board-container')
-    mainBody.appendChild(board)
-    squareWidth = board.offsetWidth/boardLength
-    for (let x=0;x<boardLength;x++) {
-        squares.push([])
-        for (let y=0;y<boardLength;y++) {
-            let tempSquare = document.createElement('div')
-            tempSquare.classList.add("board-square")
-            tempSquare.style.minWidth = squareWidth+'px'
-            tempSquare.addEventListener('mouseover',changeColor)
-            board.appendChild(tempSquare)
-            squares[x].push(board)
-        }
-    }
-}
-
 const deleteBoard = function() {
     board = document.querySelector('.board-container')
     board.remove()
@@ -50,6 +32,15 @@ const showSlider = function showSlider(boardLength) {
     sliderContainer.appendChild(newSlider)
     sliderContainer.appendChild(sizeText)
     optionsContainer.appendChild(sliderContainer)
+    let slider = document.querySelector('.slider')
+    let sliderOutput = document.querySelector('.slider-value')
+    sliderOutput.textContent = `Current Size: ${slider.value} X ${slider.value}`
+
+    slider.oninput = function() {
+        sliderOutput.textContent = `Current Size: ${slider.value} X ${slider.value}`
+        deleteBoard()
+        drawBoard(slider.value)
+  }
 }
 
 const showResetButton = function () {
@@ -84,6 +75,24 @@ const clearBoard = function () {
     drawBoard(boardLength)
 }
 
+const drawBoard = function (boardLength) {
+    const board = document.createElement('div')
+    board.classList.add('board-container')
+    mainBody.appendChild(board)
+    squareWidth = board.offsetWidth/boardLength
+    for (let x=0;x<boardLength;x++) {
+        squares.push([])
+        for (let y=0;y<boardLength;y++) {
+            let tempSquare = document.createElement('div')
+            tempSquare.classList.add("board-square")
+            tempSquare.style.minWidth = squareWidth+'px'
+            tempSquare.addEventListener('mouseover',changeColor)
+            board.appendChild(tempSquare)
+            squares[x].push(board)
+        }
+    }
+}
+
 boardLength = 16
 const squares = []
 let squresTagged = 0
@@ -94,12 +103,12 @@ const optionsContainer = document.querySelector('.options-container')
 showSlider(boardLength)
 drawBoard(boardLength)
 
-let slider = document.querySelector('.slider')
-let sliderOutput = document.querySelector('.slider-value')
-sliderOutput.textContent = `Current Size: ${slider.value} X ${slider.value}`
+// let slider = document.querySelector('.slider')
+// let sliderOutput = document.querySelector('.slider-value')
+// sliderOutput.textContent = `Current Size: ${slider.value} X ${slider.value}`
 
-slider.oninput = function() {
-    sliderOutput.textContent = `Current Size: ${slider.value} X ${slider.value}`
-    deleteBoard()
-    drawBoard(slider.value)
-  }
+// slider.oninput = function() {
+//     sliderOutput.textContent = `Current Size: ${slider.value} X ${slider.value}`
+//     deleteBoard()
+//     drawBoard(slider.value)
+//   }
